53d0c63217b9026b813b472ab2f039cce3b129ac
Joy Chopra <joy.chopra2@gmail.com>
2018-02-12 18:05:04 +0530

    Update dqn_her.py

--- baselines/deepq/dqn_her.py
+++ baselines/deepq/dqn_her.py
@@ -264,9 +264,9 @@ def learn(env,
 
             if (checkpoint_freq is not None and t > learning_starts and
                     num_episodes > 100 and t % checkpoint_freq == 0):
-                if saved_mean_reward_diff is None or mean_100ep_max_reward-mean_100ep_reward > saved_mean_reward_diff:
+                if saved_mean_reward_diff is None or mean_100ep_max_reward-mean_100ep_reward < saved_mean_reward_diff:
                     if print_freq is not None:
-                        logger.log("Saving model due to mean reward increase: {} -> {}".format(
+                        logger.log("Saving model due to mean reward difference decrease: {} -> {}".format(
                                    saved_mean_reward_diff, mean_100ep_reward))
                     U.save_state(model_file)
                     model_saved = True
