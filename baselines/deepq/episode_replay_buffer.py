b3cb5b56c2a6ac0b5af3d01172001fdee9ac497d
Joy Chopra <joy.chopra2@gmail.com>
2018-02-21 04:11:54 +0530

    episode replay buffer complete

--- baselines/deepq/episode_replay_buffer.py
+++ baselines/deepq/episode_replay_buffer.py
@@ -20,9 +20,9 @@ class ReplayBuffer(object):
 
     def __len__(self):
         return len(self._storage)
-
-    def add(self, obs_t, action, reward, obs_tp1, done):
-        data = (obs_t, action, reward, obs_tp1, done)
+    # episode is a list of 5 tuple (obs_t, action, reward, obs_tp1, done)
+    def add(self, episode):
+        data = episode
 
         if self._next_idx >= len(self._storage):
             self._storage.append(data)
@@ -31,16 +31,12 @@ class ReplayBuffer(object):
         self._next_idx = (self._next_idx + 1) % self._maxsize
 
     def _encode_sample(self, idxes):
-        obses_t, actions, rewards, obses_tp1, dones = [], [], [], [], []
+        episodes = []
         for i in idxes:
             data = self._storage[i]
-            obs_t, action, reward, obs_tp1, done = data
-            obses_t.append(np.array(obs_t, copy=False))
-            actions.append(np.array(action, copy=False))
-            rewards.append(reward)
-            obses_tp1.append(np.array(obs_tp1, copy=False))
-            dones.append(done)
-        return np.array(obses_t), np.array(actions), np.array(rewards), np.array(obses_tp1), np.array(dones)
+            episode = data
+            episodes.append(np.array(episode, copy=False))
+        return np.array(episodes)
 
     def sample(self, batch_size):
         """Sample a batch of experiences.
